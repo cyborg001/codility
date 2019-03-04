e3d0d56618bdfaa492d5f82908cb7586b0ce6e75
cyborg001 <cgrs27@gmail.com>
2019-03-04 18:28:32 -0400

    aqui va dando un 66%

--- OddOccurrencesInArray.py
+++ OddOccurrencesInArray.py
@@ -2,6 +2,17 @@
 """
 Created on Mon Mar  4 07:12:23 2019
 
-@author: el_in
+@author: cyborg001
 """
 
+def solution(A):
+    while True:
+        e = A.pop()
+        if e in A:
+            A.remove(e)
+        else:
+            return e
+        print(A)
+            #return e
+A = [9, 3, 9, 3, 9, 7, 9]
+print(solution(A))
