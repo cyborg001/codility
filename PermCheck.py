7fb6d694a7ab5a57b88d71fc74457d5d743d6f90
cyborg001 <cgrs27@gmail.com>
2019-03-06 18:07:06 -0400

    listo al 100%

---
+++ PermCheck.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 16:44:35 2019
+
+***PermCheck: programa que chequea si un conjunto
+***de numeros es una permutacion
+
+@author: cyborg001
+"""
+
