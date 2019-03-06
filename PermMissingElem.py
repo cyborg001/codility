f4c6b7768f4f59101665151761dd25c2994d4610
cyborg001 <cgrs27@gmail.com>
2019-03-06 11:58:01 -0400

    al 100%

---
+++ PermMissingElem.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 10:13:46 2019
+PermMissingElem: programa que encuentra el 
+elemento faltante de una serie ordenada de numeros
+naturales
+@author: cyborg001
+"""
+
