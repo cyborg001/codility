2e33b2c61dd00270e91949fada36bc50c307c6dd
cyborg001 <cgrs27@gmail.com>
2019-03-06 18:09:10 -0400

    listo al 100%

--- PermCheck.py
+++ PermCheck.py
@@ -8,3 +8,30 @@ Created on Wed Mar  6 16:44:35 2019
 @author: cyborg001
 """
 
+def solution(A):
+   
+        
+    if len(A)==0:
+        return 0
+    if len(A)==1:
+        if A[0]==1:
+            return 1
+        return 0
+    k = max(A)
+    if k-1 != len(A) or A != list(set(A)) :
+        return 0
+    
+    ser2 = int((k)*(k+1)/2)
+    tot = sum(A)
+    
+    if tot == ser2:
+        return 1
+    else:
+        return 0
+
+
+A = []
+
+def serie(k,i):
+    return int((k-i)*(k+i+1)/2)
+print(solution(A))
