fa77ffb0b923cede9387cfbe4c6a52d394325445
cyborg001 <cgrs27@gmail.com>
2019-03-06 09:04:05 -0400

    completado 100%

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -4,4 +4,15 @@ Created on Tue Mar  5 21:00:57 2019
 CyclicRotation
 @author: cyborg001
 """
+v = [n for n in range(1,10)]
 
+def solution(v,r):
+    length = len(v)
+    vtemp = length*[0]
+    for i in range(length):
+        posi = (i+r)%length
+        print(i+1,posi)
+        vtemp[posi]=v[i]
+    return vtemp
+
+print(solution(v,3))
